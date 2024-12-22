9dfdf25ad12780bea21f8554278859233669d28e
LucasRodriguesCartaxo <lucas.cartaxo@sptech.school>
2024-12-22 03:58:31 -0300

    padoru

---
+++ aula31(funções em js)/funções.js
@@ -0,0 +1,11 @@
+function saudacao(nome) {
+    console.log(`bom dia ${nome}`);
+}
+
+saudacao('lucas');
+
+function time(time){
+    console.log(`o melhor time do brasil é o ${time}`)
+}
+
+time('SaoPaulo');
