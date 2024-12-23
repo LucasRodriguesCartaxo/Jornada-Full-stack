21888b518ba4e1969ecaffba6fe7d67d1595747d
LucasRodriguesCartaxo <lucas.cartaxo@sptech.school>
2024-12-23 00:50:03 -0300

    função

--- aula31(funções em js)/funções.js
+++ aula31(funções em js)/funções.js
@@ -16,7 +16,7 @@ function cs(skthedram) {
 cs("fallen,coldzera,taco,fer,fnx");
 //-----------------------------------------------------------------------------------------------------------------------
 
-function sobreNome(sobreNome){ //funcar recebe um nome
+function sobreNome(sobreNome) { //funcar recebe um nome
     return `seu sobrenome é ${sobreNome}` //passando oque sera retornado
 }
 const variavel = sobreNome("rodrigues"); // declarando o conteudo que vai ter dentro da funcao
@@ -24,24 +24,31 @@ console.log(variavel) //exibindo a variavel que foi criada -> no console vai apa
 //-----------------------------------------------------------------------------------------------------------------------
 
 // vamos criar uma funcção que faz a soma de dois valores;
-function soma (a,b){  // declarando que a função ira receber dois parametros;
-    return a+b    // dentro do corpo da funcao estou dizendo que ira retornar a soma desses dois valores
+function soma(a, b) {  // declarando que a função ira receber dois parametros;
+    return a + b    // dentro do corpo da funcao estou dizendo que ira retornar a soma desses dois valores
 }
-const numeros = soma(1,3); // estou chamando a função soma, e estou passando que o valor do parametro a= 1 & b=3
+const numeros = soma(1, 3); // estou chamando a função soma, e estou passando que o valor do parametro a= 1 & b=3
 console.log(numeros) // O valor retornado pela função (4) é armazenado na constante numeros
 //-----------------------------------------------------------------------------------------------------------------------
 
 // criando uma funcao que ira fazer a multiplicacão dos valores;
-function multiplicacao(x,y){
+function multiplicacao(x, y) {
     return x * y
 }
-const multi = multiplicacao(10,20);
+const multi = multiplicacao(10, 20);
 console.log(multi)  // o valor 200 foi exibido no OUTPUT(console)
 //-----------------------------------------------------------------------------------------------------------------------
 //Criando uma funcção anonima, vou criar uma função que faz a raiz quadrado de um numero;
-const raizQuadrada = function (n){
+const raizQuadrada = function (n) {
     return n ** 0.5;
 }; // nesse modelo é obrigatorio terminar a função com ponto e virgula
-console.log(raizQuadrada(9)); 
+console.log(raizQuadrada(9));
 console.log(raizQuadrada(16));
 console.log(raizQuadrada(25));
+//-----------------------------------------------------------------------------------------------------------------------
+function subtracao(a, b) {
+    return a - b
+}
+const conta = subtracao(50, 30);
+console.log(conta)
+
